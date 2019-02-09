300b32553ab79ef001eae5af61df9cfb71d1acfe
Igor <bushemi2001@gmail.com>
2019-02-10 01:33:26 +0200

    just did a little refactoring

--- src/test/java/de/comparus/opensource/longmap/LongMapImplTest.java
+++ src/test/java/de/comparus/opensource/longmap/LongMapImplTest.java
@@ -12,94 +12,97 @@ import static java.util.Objects.isNull;
 
 @RunWith(JUnit4.class)
 public class LongMapImplTest {
-    public static final int START = 0;
-    public static final int FINISH = 50000;
+    private static final int START = 0;
+    private static final int FINISH = 50000;
     Map<Long, String> map = new HashMap<>();
     LongMap<String> map2 = new LongMapImpl<>();
 
     @Test
-
-    public void putHashMap() throws Exception {
-//        System.out.println("start");
-//        long start = System.nanoTime();
+    public void putHashMap() {
+        System.out.println("start");
         long start = System.currentTimeMillis();
-        LongStream.range(START, FINISH).forEach(e -> map.put(e, "one" + e));
-//        long finish = System.nanoTime();
+        fillTheHMap();
         long finish = System.currentTimeMillis();
-//        System.out.println("finish-start = " + (finish - start));
+        System.out.println("finish-start = " + (finish - start));
         System.out.println("map.size() = " + map.size());
     }
 
     @Test
-    public void putMyMap() throws Exception {
-//        System.out.println("start");
-//        long start = System.nanoTime();
+    public void putMyMap() {
+        System.out.println("start");
         long start = System.currentTimeMillis();
-        LongStream.range(START, FINISH).forEach(e -> map2.put(e, "one" + e));
-//        long finish = System.nanoTime();
+        fillMyMap();
         long finish = System.currentTimeMillis();
-//        System.out.println("finish-start = " + (finish - start));
+        System.out.println("finish-start = " + (finish - start));
         System.out.println("map2.size() = " + map2.size());
     }
 
     @Test
-    public void getFromHashMap() throws Exception {
-        putHashMap();
+    public void getFromHashMap() {
+        fillTheHMap();
         System.out.println("start");
-//        long start = System.nanoTime();
         long start = System.currentTimeMillis();
-        LongStream.range(START, FINISH).forEach(e -> map.get(e));
-//        long finish = System.nanoTime();
+        LongStream.range(START, FINISH).forEach(e -> {
+            String obj = map.get(e);
+            if (isNull(obj))
+                throw new RuntimeException("Fail from getFromMyMap");
+        });
         long finish = System.currentTimeMillis();
         System.out.println("finish-start = " + (finish - start));
     }
 
     @Test
-    public void getFromMyMap() throws Exception {
-        putMyMap();
+    public void getFromMyMap() {
+        fillMyMap();
         System.out.println("start");
-//        long start = System.nanoTime();
         long start = System.currentTimeMillis();
         LongStream.range(START, FINISH).forEach(e -> {
             String obj = map2.get(e);
             if (isNull(obj))
                 throw new RuntimeException("Fail from getFromMyMap");
         });
-//        long finish = System.nanoTime();
         long finish = System.currentTimeMillis();
         System.out.println("finish-start = " + (finish - start));
     }
 
     @Test
-    public void remove() throws Exception {
+    public void remove() {
     }
 
     @Test
-    public void isEmpty() throws Exception {
+    public void isEmpty() {
     }
 
     @Test
-    public void containsKey() throws Exception {
+    public void containsKey() {
     }
 
     @Test
-    public void containsValue() throws Exception {
+    public void containsValue() {
     }
 
     @Test
-    public void keys() throws Exception {
+    public void keys() {
     }
 
     @Test
-    public void values() throws Exception {
+    public void values() {
     }
 
     @Test
-    public void size() throws Exception {
+    public void size() {
     }
 
     @Test
-    public void clear() throws Exception {
+    public void clear() {
+    }
+
+    private void fillTheHMap() {
+        LongStream.range(START, FINISH).forEach(e -> map.put(e, "one" + e));
+    }
+
+    private void fillMyMap() {
+        LongStream.range(START, FINISH).forEach(e -> map2.put(e, "one" + e));
     }
 
 }
